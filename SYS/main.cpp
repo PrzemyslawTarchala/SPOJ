7ab8f6f65b6a0afba907f97a11f79e72b4c2433a
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2023-08-14 14:58:28 +0200

    Add SYS task from SPOJ

---
+++ SYS/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void przeksztalcNaHex(int liczba, char wynikk){
+    char wynik[10];
+    atol(itoa(liczba, wynik, 16));
+    cout << wynik << " ";
+}
+
+void przeksztalcNaJedenastkowy(int liczba){
+    char wynik[10];
+    atol(itoa(liczba, wynik, 11));
+    cout << wynik << endl;
+}
+
+int main()
+{
+    char *wynikk = new (char);
+    *wynikk = 10;
+    cout << wynikk;
+    int liczbaPrzypadkow, liczbaDoKonwersji;
+    cin >> liczbaPrzypadkow;
+
+    for (int i = 0; i < liczbaPrzypadkow; ++i){
+        cin >> liczbaDoKonwersji;
+        przeksztalcNaHex(liczbaDoKonwersji, wynikk);
+        przeksztalcNaJedenastkowy(liczbaDoKonwersji);
+    }
+    return 0;
+}
+
+
+
+
+
+
+/*
+void przeksztalcNaHex(int liczba){
+    char wynik[10];
+    atol(itoa(liczba, wynik, 16));
+    cout << wynik << " ";
+}
+
+void przeksztalcNaJedenastkowy(int liczba){
+    char wynik[10];
+    atol(itoa(liczba, wynik, 11));
+    cout << wynik << endl;
+}
+
+int main()
+{
+    char *wynikk = new (char);
+    int liczbaPrzypadkow, liczbaDoKonwersji;
+    cin >> liczbaPrzypadkow;
+
+    for (int i = 0; i < liczbaPrzypadkow; ++i){
+        cin >> liczbaDoKonwersji;
+        przeksztalcNaHex(liczbaDoKonwersji);
+        przeksztalcNaJedenastkowy(liczbaDoKonwersji);
+    }
+    return 0;
+}
+
+*/
+
+
