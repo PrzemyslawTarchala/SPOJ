d410e25964a370f052a651c3654b7f94af8fe2e9
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2023-08-14 15:01:30 +0200

    Little changes in SYS task

--- SYS/main.cpp
+++ SYS/main.cpp
@@ -32,37 +32,3 @@ int main()
 }
 
 
-
-
-
-
-/*
-void przeksztalcNaHex(int liczba){
-    char wynik[10];
-    atol(itoa(liczba, wynik, 16));
-    cout << wynik << " ";
-}
-
-void przeksztalcNaJedenastkowy(int liczba){
-    char wynik[10];
-    atol(itoa(liczba, wynik, 11));
-    cout << wynik << endl;
-}
-
-int main()
-{
-    char *wynikk = new (char);
-    int liczbaPrzypadkow, liczbaDoKonwersji;
-    cin >> liczbaPrzypadkow;
-
-    for (int i = 0; i < liczbaPrzypadkow; ++i){
-        cin >> liczbaDoKonwersji;
-        przeksztalcNaHex(liczbaDoKonwersji);
-        przeksztalcNaJedenastkowy(liczbaDoKonwersji);
-    }
-    return 0;
-}
-
-*/
-
-
